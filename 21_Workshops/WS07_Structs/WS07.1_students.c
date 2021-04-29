133c949b96779973ef222e1b0bac29aed00fc84b
SchlJori <jorian.schlunegger@windowslive.com>
2021-04-29 22:26:42 +0200

    SW10

--- 21_Workshops/WS07_Structs/WS07.1_students.c
+++ 21_Workshops/WS07_Structs/WS07.1_students.c
@@ -1,32 +1,82 @@
 #include <stdio.h>
 
+
 #define MAX_NAME_LENGTH 30
 #define NUM_GRADES 6
 #define NUM_STUDENTS 5
 
-struct Students {
-	char firstname[MAX_NAME_LENGHT];
-	char lastname[MAX_NAME_LENGHT];
+struct Student {
+	char firstname[MAX_NAME_LENGTH];
+	char lastname[MAX_NAME_LENGTH];
 	float grades[NUM_GRADES];
 	float average;
 };
 	
 struct Student students[NUM_STUDENTS] = {	
-[0].firstname = "Tony",	[0].lastname = "Stark",
-[1].firstname = "Peter",	[1].lastname = "Parker",
-[2].firstname = "Bruce",	[2].lastname = "Banner",
-[3].firstname = "Steve",	[3].lastname = "Rodgers",
-[4].firstname = "Natasha",	[4].lastname = "Romanoff",
+[0].firstname = "Tony",	[0].lastname = "Stark",	[0].grades = {6.0, 5.8, 5.9, 6.0, 5.9, 6.0},
+[1].firstname = "Peter",	[1].lastname = "Parker",	[1].grades = {4.5, 4.8, 5.8, 3.2, 5.9, 5.2},
+[2].firstname = "Bruce",	[2].lastname = "Banner",	[2].grades = {5.5, 2.8, 5.3, 4.8, 5.4, 4.5},
+[3].firstname = "Steve",	[3].lastname = "Rodgers",	[3].grades = {4.9, 3.9, 5.8, 2.3, 4.3, 5.4},
+[4].firstname = "Natasha",	[4].lastname = "Romanoff",	[4].grades = {5.3, 5.2, 5.1, 5.7, 4.9, 5.9} };
 
 
-struct students myStudents;
-	myStudents.firstName.Tony
-	myStudents.firstName.Peter
-	myStudents.firstName.Bruce
-	myStudents.firstName.Steve
-	myStudents.firstName.Natasha
+void StudentGradeAverage( struct Student *Stud)
+{
+	float SumGrades = 0;
+	for(int k = 0; k < NUM_GRADES; k++)
+	{
+		SumGrades = SumGrades + Stud->grades[k];
+	}
+	Stud->average = SumGrades/NUM_GRADES;
+	printf(" Average: %.2f\n", Stud->average);
+}
+
+void NameOfStudents( struct Student *name)
+{
+	printf(" %s, \t %s   \t", name->lastname, name->firstname);
+}
+	
+void GradeStudent( struct Student *allGrades)
+{
+	for(int n = 0; n < NUM_GRADES; n++)
+	{
+		printf(" %.2f,", allGrades->grades[n]);
+	}
+}
 
 
-	struct firstName;
-	char lastName;
-	float grades;
+
+
+int main(int argc, char *argv[] ) 
+{
+	float ClassAverage = 0;
+	for( int i = 0; i < NUM_STUDENTS; i++) 
+	{
+		NameOfStudents(&students[i]);
+		GradeStudent(&students[i]);
+		StudentGradeAverage(&students[i]);
+		ClassAverage += students[i].average;
+	}
+	ClassAverage = ClassAverage/NUM_STUDENTS;
+	printf("\n Class Average: %.2f\n", ClassAverage);
+	return 0;
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 40_Exercices/11_switch/coffeeMaker.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// State Machine 
+typedef enum {
+	HEATING = 1,
+	SHOWMENU,
+	MILLING,
+	COFFEEMAKING,
+	CLEANING,
+	SHUTDOWN,
+	ERROR
+} CoffeeMakerStatus;
+
+
+CoffeeMakerStatus status = HEATING;
+	
+// function prototypes
+void ShowMenu( void );
+int HeatingDone( void );
+
+
+int main(void) {
+
+	// endless loop
+	int run = 1;
+	while(run == 1) 
+	{
+		//CheckSensors();
+		switch(status)
+		{
+			case HEATING:
+				if( HeatingDone() )
+				{
+					status = SHOWMENU;
+				}
+				break;
+				
+			case SHOWMENU:
+				ShowMenu();
+				break;
+		
+			case MILLING:
+				printf("milling ... done.\n");
+				status = COFFEEMAKING;
+				break;
+				
+			case COFFEEMAKING:
+				printf("mmmmh... smells good! Done.\n");
+				status = CLEANING;
+				break;
+			
+			case CLEANING:
+				printf("cleaning.... done.\n");
+				status = SHOWMENU;
+				break;
+		
+			// add all cases
+		
+			case SHUTDOWN:
+				printf("Shutting down.\n");
+				run = 0;
+				break;
+
+
+			default:
+				printf("Coffee Maker in Error State.\n");
+				status = SHUTDOWN;
+				break;
+		}
+	}
+
+	return 0;
+}
+
+
+
+static int heatingCounter = 0;
+int heatingLimit = 10;
+
+// returns 1 if heating done, 0 otherwise.
+int HeatingDone(void){
+	int done = 0;
+	//sleep(1);  // sleep for 1 s.
+ 	usleep(500000);  // sleep n microsecond
+	printf("#");
+	fflush(stdout);
+	heatingCounter++;
+	if(heatingCounter >= heatingLimit)
+	{
+		// reset the counter!! 
+		heatingCounter = 0;
+		done = 1;
+		printf(" Heating Completed.\n");
+	}
+	return done;
+}
+	
+
+
+void ShowMenu(void ){
+	int menuSelected = 0;
+	printf("Select from the following Options:\n");
+	printf("1:  Re-heat\n");
+	printf("2:  Make Coffee\n");
+	printf("9: shutdown\n");
+	// blocking read!!
+	scanf("%d", &menuSelected);
+	switch(menuSelected)
+	{
+		case 1:
+			status = HEATING;
+			break;
+		case 2:
+			status = MILLING;
+			break;
+		
+		case 9:
+			status = SHUTDOWN;
+			break;
+	
+		default:
+			printf("Invalid selection.\n");
+			status = SHOWMENU;
+			break;
+	}
+	/*
+	if( menuSelected == 1 ){
+		status = HEATING;
+	} else if( menuSelected == 2 ) {
+		status = MILLING;
+	} else if( menuSelected == 9 ) {
+		status = SHUTDOWN;
+	} else {
+		printf("Invalid selection.\n");
+		status = SHOWMENU;
+	}
+	*/
+}	
+
+
+
